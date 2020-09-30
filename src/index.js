1820ea251d2403a25f5181d262a7403a13de21a8
Artur <arturexpone@yandex.ru>
2020-09-30 20:15:02 +0300

    feat: resolve max function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-    return 0;
+    return !Array.isArray(array) || array.length === 0 ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-    return 0;
+  if (!Array.isArray(array) || array.length === 0) return 0;
+  return array.reduce((acc, num) => num + 0, 0) / array.length - 1;
 }
